0e2634201f3d57eccb18a916ad087bed62abd7eb
Anthony Quinones <quinoant@gmail.com>
2023-07-11 15:11:41 -0500

    specializing the command

--- practice.py
+++ practice.py
@@ -1,16 +1,20 @@
 import praw;
+import sys;
 
-def get_headlines(reddit):
+def get_headlines(reddit, num_posts, subreddit):
       headlines = set()
-      for submission in reddit.subreddit('politics').hot(limit=None):
+      counter = num_posts
+      for submission in reddit.subreddit(subreddit).top(limit=None):
             print(submission.title)
-            print(submission.id)
-            print(submission.author)
-            print(submission.score)
-            print(submission.url)
-            print(submission.upvote_ratio)
+            # print(submission.id)
+            # print(submission.author)
+            # print(submission.score)
+            # print(submission.url)
+            # print(submission.upvote_ratio)
             headlines.add(submission.title)
-            break
+            num_posts-=1
+            if(num_posts<=0):
+                break
       print(len(headlines))
             
         
@@ -23,7 +27,12 @@ def main():
         client_secret = "RbwT9McsSUQzZ2Id84TejuVyO75Hvg",
         user_agent = the_user_agent
     )
-    get_headlines(reddit)
+    #get_headlines(reddit)
+    print("Hello and Welcome to the Reddit API User Expierience!")
+    print("To see the top x posts in subreddit y use check x in y")
+    print(sys.argv[1])
+    if(sys.argv[1] == "check"):
+          get_headlines(reddit,int(sys.argv[2]),sys.argv[3])
 
 if __name__ == '__main__':
 	main()
